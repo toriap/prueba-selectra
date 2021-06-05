ea7698a01a36e8fb2876ef03213c96c785237faa
Christopher Pereira <ggerardogs@gmail.com>
2021-06-05 05:08:35 +0200

    archivo index creado y arreglado

---
+++ src/js/add.js
@@ -0,0 +1,7 @@
+var regexName = /^[a-z ,.'-]+$/i;
+var regexNumber = /(6)([0-9]){8}/g;
+var regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/i;
+
+window.onload = function (){
+  
+}
